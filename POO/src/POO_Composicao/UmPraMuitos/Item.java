f22588a853b3c9eb92811ac94ee941dc46b585f9
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-19 21:45:23 -0300

    Create UmPraMuitos

---
+++ POO/src/POO_Composicao/UmPraMuitos/Item.java
@@ -0,0 +1,14 @@
+package POO_Composicao.UmPraMuitos;
+
+public class Item {
+    String nome;
+    int quantidade;
+    double preco;
+    Compra compra;
+
+    Item(String nome, int quantidade, double preco){
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+}
