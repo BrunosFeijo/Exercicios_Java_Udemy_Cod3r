4ef3efd3adfb52e40d0a93bba4d1395725771e3a
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-23 21:59:56 -0300

    Herança

--- POO/src/POO/Heranca/Desafio/Ferrari.java
+++ POO/src/POO/Heranca/Desafio/Ferrari.java
@@ -4,21 +4,6 @@ public class Ferrari extends Carro{
     int delta = 15;
     Ferrari(int velocidadeMaxima){
         super(velocidadeMaxima);
-    }
-    @Override
-    void acelerar() {
-        if (velocidade + delta <= VELOCIDADE_MAXIMA){
-            velocidade += delta;
-        }else {
-            velocidade = VELOCIDADE_MAXIMA;
-        }
-    }
-    @Override
-    void frear() {
-        if (velocidade - delta >= 0){
-            velocidade -= delta;
-        }else{
-            velocidade = 0;
-        }
+        super.delta = this.delta;
     }
 }
