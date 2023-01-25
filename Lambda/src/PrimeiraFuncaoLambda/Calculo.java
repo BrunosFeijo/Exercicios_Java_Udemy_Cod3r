3ab5b3b8a5f7c7509a5992abfd8f11ea6fbd538d
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-25 20:30:11 -0300

    Lambda

--- Lambda/src/PrimeiraFuncaoLambda/Calculo.java
+++ Lambda/src/PrimeiraFuncaoLambda/Calculo.java
@@ -2,4 +2,7 @@ package PrimeiraFuncaoLambda;
 @FunctionalInterface
 public interface Calculo {
     double executar(double a, double b);
+
+    //Ainda é possível crir um método default
+    //Também é possível criar um método static
 }
