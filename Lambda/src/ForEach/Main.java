c10d53599a55e6beac4e2481beddcd248bd11cf2
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-25 21:22:16 -0300

    Lambda

--- Lambda/src/ForEach/Main.java
+++ Lambda/src/ForEach/Main.java
@@ -1,6 +1,7 @@
 package ForEach;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,10 +11,18 @@ public class Main {
         for (String nome : aprovados) {
             System.out.println(nome);
         }
-        System.out.println("\nCom Lambdas...");
+        System.out.println("\nCom Lambdas #01...");
         aprovados.forEach(nome -> System.out.println(nome + "!!!"));
+        System.out.println("\nCom Lambdas #02...");
+        aprovados.forEach(nome -> imprimir(nome));
 
-        System.out.println("\nMethod Reference...");
+        System.out.println("\nMethod Reference #01...");
         aprovados.forEach(System.out::println);
+        System.out.println("\nMethod Reference #02...");
+        aprovados.forEach(Main::imprimir);
+    }
+
+    static void imprimir(String nome){
+        System.out.println(nome + "***");
     }
 }
