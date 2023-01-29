30fa8605073e92d2c17f8635e6a82aa35cffccfd
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-29 19:10:05 -0300

    Enum

--- Enumeradores/src/Enum_Metodos/Main.java
+++ Enumeradores/src/Enum_Metodos/Main.java
@@ -3,7 +3,6 @@ package Enum_Metodos;
 public class Main {
     public static void main(String[] args) {
 //        DiaSemana dias[] = DiaSemana.values();
-
         for(DiaSemana dia: DiaSemana.values()){
             System.out.println(dia);
         }
