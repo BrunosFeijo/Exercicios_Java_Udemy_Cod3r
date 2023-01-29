034990401d70967ab0c44943d8c150de8cc2da94
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-29 14:47:47 -0300

    Enum

---
+++ Enumeradores/src/DiaSemanaConstantes.java
@@ -0,0 +1,9 @@
+public class DiaSemanaConstantes {
+    public final String SEGUNDA = "Segunda-feira";
+    public final String TERCA ="Terça-feira";
+    public final String QUARTA = "Quarta-feira";
+    public final String QUINTA = "Quinta-feira";
+    public final String SEXTA = "Sexta-feira";
+    public final String SABADO = "Sábado";
+    public final String DOMINGO = "Domingo";
+}
