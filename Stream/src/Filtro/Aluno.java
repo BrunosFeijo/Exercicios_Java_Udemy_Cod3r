2e776bf4d458c0c6373803cc95b4b601adc4c333
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-29 21:55:41 -0300

    Stream
    filter

---
+++ Stream/src/Filtro/Aluno.java
@@ -0,0 +1,11 @@
+package Filtro;
+
+public class Aluno {
+    public String nome;
+    public double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+}
