2b2f2d06932fb11673b7b83c6771239b647e0d00
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-29 21:29:04 -0300

    Stream
    map

--- Stream/src/DesafioMap/Main.java
+++ Stream/src/DesafioMap/Main.java
@@ -1,10 +1,31 @@
 package DesafioMap;
-
 //1. Converter número do Array para binário ... 6 => "110"
 //2. Inverter a string... "110" => "011"
 //3. Converter de volta para inteiro... "011" => 3
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Function<Integer, String> binario = Integer::toBinaryString;
+        UnaryOperator<String> inverter = Main::reverseString;
+        Function<String, Integer> decimal = x -> Integer.parseInt(x, 2);
+
+        nums.stream()
+                .map(binario)
+                .map(inverter)
+                .map(decimal)
+                .forEach(System.out::println);
 
     }
+
+    public static String reverseString(String input) {
+        StringBuilder teste = new StringBuilder(input);
+        return teste.reverse().toString();
+    }
+
 }
