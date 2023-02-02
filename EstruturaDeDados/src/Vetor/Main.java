6ee2561525154a60cda71b9a30f1309a4441435a
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-02-02 14:22:08 -0300

    Estrutura de Dados

--- EstruturaDeDados/src/Vetor/Main.java
+++ EstruturaDeDados/src/Vetor/Main.java
@@ -3,16 +3,22 @@ package Vetor;
 public class Main {
     public static void main(String[] args) {
         //Testando versão somente String
-        
+        Vetor v = new Vetor(5);
+
+        v.adiciona("Bruno");
+        v.adiciona("1");
+        v.adiciona("true");
+
+        System.out.println(v);
 
         System.out.println();
         //Testando versão com Object
-        VetorGenericoUsandoObject v = new VetorGenericoUsandoObject(5);
+        VetorGenericoUsandoObject vObject = new VetorGenericoUsandoObject(5);
 
-        v.adiciona("Bruno");
-        v.adiciona(1);
-        v.adiciona(true);
+        vObject.adiciona("Bruno");
+        vObject.adiciona(1);
+        vObject.adiciona(true);
 
-        System.out.println(v.toString());
+        System.out.println(vObject);
     }
 }
