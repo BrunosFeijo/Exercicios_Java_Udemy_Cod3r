0ad27235343a2c02d3f02a49a93d3eb5e13d7e75
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-02-03 20:31:46 -0300

    Estrutura de Dados

--- EstruturaDeDados/src/Pilhas/Exercicios/E01.java
+++ EstruturaDeDados/src/Pilhas/Exercicios/E01.java
@@ -4,5 +4,33 @@ package Pilhas.Exercicios;
 // * Se o número for ímpar, desempilhe um número da pilha. Caso a pilha esteja vazia, mostre uma mensagem
 // * Se ao final do programa a pilha não estiver vazia, desempilhe todos os elementos, imprimindo-os na tela
 
+import Pilhas.Pilha;
+
+import java.util.Random;
+
 public class E01 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Pilha<Integer> pilha = new Pilha<>(10);
+        int num = -1;
+
+        for (int i = 0; i < 10; i++) {
+            num = random.nextInt(100);
+            System.out.println(num);
+            if ((num % 2) == 0) {
+                pilha.empilha(num);
+            } else if (pilha.estaVazia()) {
+                System.out.println("Não foi possivel desempilhar nenhum elemento. Pilha vazia.");
+            } else {
+                pilha.desempilha();
+            }
+        }
+
+        if (!pilha.estaVazia()) {
+            for (int i = pilha.tamanho() - 1; i >= 0; i--) {
+                System.out.println(pilha.topo());
+                pilha.desempilha();
+            }
+        }
+    }
 }
