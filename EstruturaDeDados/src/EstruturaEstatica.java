7d0330b65f9c90737b3e940e9d7ac710d26a1279
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-02-03 02:39:04 -0300

    Pilha

---
+++ EstruturaDeDados/src/EstruturaEstatica.java
@@ -0,0 +1,83 @@
+public class EstruturaEstatica<T> {
+    protected T[] elementos;
+    protected int tamanho;
+
+    public EstruturaEstatica(int capacidade) {
+        this.elementos = (T[]) new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    public EstruturaEstatica() {
+        this(10);
+    }
+
+    public boolean estaVazia() {
+        return this.tamanho == 0;
+    }
+
+    protected boolean adiciona(T elemento) {
+        this.aumentaCapacidade();
+
+        this.elementos[this.tamanho] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
+    protected boolean adiciona(int posicao, T elemento) {
+        if (posicao < 0 || posicao > tamanho) throw new IllegalArgumentException("Posição Inválida");
+
+        this.aumentaCapacidade();
+
+        for (int i = tamanho; i >= posicao; i--) {
+            this.elementos[i] = elementos[i - 1];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
+    protected void remove(int posicao) {
+        if (posicao < 0 || posicao > tamanho) throw new IllegalArgumentException("Posição Inválida");
+        for (int i = posicao; i < tamanho - 1; i++) {
+            elementos[i] = elementos[i + 1];
+        }
+        this.tamanho--;
+    }
+
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            T[] elementosNovos = (T[]) new Object[tamanho * 2];
+            for (int i = 0; i < tamanho - 1; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+        }
+
+        s.append("]");
+
+        return s.toString();
+    }
+
+}
