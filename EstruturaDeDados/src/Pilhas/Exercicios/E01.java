dcfaf942ce9bf5aac641fc1f7fb8179ece9079a0
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-02-03 02:54:17 -0300

    Estrutura de Dados

---
+++ EstruturaDeDados/src/Pilhas/Exercicios/E01.java
@@ -0,0 +1,8 @@
+package Pilhas.Exercicios;
+//Escreva um programa que leia 10 números. Para cada número lido, verifique e codifique de acordo com as regras a seguir:
+// * Se o número for par, empilhe na pilha;
+// * Se o número for ímpar, desempilhe um número da pilha. Caso a pilha esteja vazia, mostre uma mensagem
+// * Se ao final do programa a pilha não estiver vazia, desempilhe todos os elementos, imprimindo-os na tela
+
+public class E01 {
+}
--- EstruturaDeDados/src/Pilhas/Pilha.java
+++ EstruturaDeDados/src/Pilhas/Pilha.java
@@ -3,5 +3,25 @@ package Pilhas;
 import EstruturaEstatica.EstruturaEstatica;
 
 public class Pilha<T> extends EstruturaEstatica<T> {
+    public Pilha() {
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+
+    public void empilha(T elementa) {
+        super.adiciona(elementa);
+    }
+
+    public T topo() {
+        if (this.estaVazia()) return null;
+        return this.elementos[tamanho - 1];
+    }
+    public T desempilha(){
+        if(this.estaVazia()) return null;
+        return this.elementos[--tamanho];
+    }
 
 }
