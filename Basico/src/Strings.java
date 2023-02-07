f8b240e1077c946484ba604aabacd0ddf5fecba5
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-02-07 12:30:25 -0300

    Basico
    String - ArrayChar

---
+++ Basico/src/Strings.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Strings {
+    public static void main(String[] args) {
+        String texto = "Bruno";
+
+        for (int i =0;i < texto.length();i++){
+            System.out.print(texto.charAt(i));
+        }
+
+        System.out.println();
+        char text[] = new char[4];
+        texto.getChars(0,4,text,0);
+        System.out.println(text);
+
+        byte textoByte[] = new byte[4];
+        texto.getBytes(0, 4, textoByte, 0);
+        System.out.println(Arrays.toString(textoByte));
+
+        char textoChar[] = texto.toCharArray();
+        System.out.println(textoChar);
+    }
+}
