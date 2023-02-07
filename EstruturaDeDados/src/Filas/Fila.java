f6b839b1b72ac7b4418a8a49abf07870afb658c9
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-02-06 22:08:22 -0300

    Estrutura de Dados
    Fila

---
+++ EstruturaDeDados/src/Filas/Fila.java
@@ -0,0 +1,36 @@
+package Filas;
+
+import EstruturaEstatica.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+    public Fila() {
+    }
+
+    public void enfileirar(T elemento) {
+        super.adiciona(elemento);
+    }
+
+    public void enfileirar(int posicao, T elemento) {
+        super.adiciona(posicao, elemento);
+    }
+    public T espiar(){
+        if (this.estaVazia()) return null;
+        return this.elementos[0];
+    }
+    // A + B + C + D + E -> Remove o primeiro termo (0)
+    // B + C + D + E
+    public T desenfileirar(){
+        if (this.estaVazia()) throw new IllegalArgumentException("Fila está vazia");
+        for (int i = 0; i < tamanho - 1;i++){
+            elementos[i] = elementos[i+1];
+        }
+        this.tamanho--;
+        return this.elementos[tamanho];
+    }
+
+}
