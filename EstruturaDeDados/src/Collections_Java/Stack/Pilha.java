83eceeb10719e335bb47298a4d5966b467d8aa58
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-06-11 18:23:59 -0300

    Stack

---
+++ EstruturaDeDados/src/Collections_Java/Stack/Pilha.java
@@ -0,0 +1,21 @@
+package Collections_Java.Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class Pilha {
+    public static void main(String[] args) {
+        Stack<String> livros = new Stack<>();
+        livros.add("O Pequeno Príncipe");
+        livros.addElement("Don Quixote");
+        livros.push("O Hobbit");
+        for (String livro: livros){
+            System.out.println(livro);
+        }
+        System.out.println(livros.pop());
+        System.out.println(livros.pop());
+        System.out.println(livros.pop());
+
+    }
+}
