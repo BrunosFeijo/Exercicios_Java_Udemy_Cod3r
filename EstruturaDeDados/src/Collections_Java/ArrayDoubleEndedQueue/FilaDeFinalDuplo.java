6017ad9960f456e12abb4e01c92462948afc9714
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-06-11 18:02:02 -0300

    Fila com Final Duplo

--- EstruturaDeDados/src/Collections_Java/ArrayDoubleEndedQueue/FilaDeFinalDuplo.java
+++ EstruturaDeDados/src/Collections_Java/ArrayDoubleEndedQueue/FilaDeFinalDuplo.java
@@ -13,6 +13,19 @@ public class FilaDeFinalDuplo {
         pilha.offerFirst(0);
         pilha.offerLast(4);
 
+
         System.out.println(pilha);
+        System.out.println("Início da pilha: " + pilha.getFirst());
+        System.out.println("Fim da pilha: " + pilha.getLast());
+        System.out.println("Removido do inicio (pop): " + pilha.pop());
+        System.out.println("Removido do inicio (poll): " + pilha.poll());
+        System.out.println("Removido do fim : " + pilha.pollLast());
+        System.out.println("Removido do fim : " + pilha.removeLast());
+
+        System.out.println("Último removido: " + pilha.pop());
+        System.out.println("Pilha vazia: " + pilha.poll()); // null
+//        System.out.println("Pilha vazia: " + pilha.removeFirst()); // exceção
+//        System.out.println("Pilha vazia: " + pilha.pop()); // exceção
+
     }
 }
