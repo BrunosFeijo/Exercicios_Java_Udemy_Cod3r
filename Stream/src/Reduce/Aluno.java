22d6f698f02a153cbfb20e543dc6d4beca8d66fc
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-08-08 14:04:14 -0300

    stream

---
+++ Stream/src/Reduce/Aluno.java
@@ -0,0 +1,11 @@
+package Reduce;
+
+public class Aluno {
+    String nome;
+    double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+}
