873fd8f15a7ea7298ecce60435a56d8264192ead
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-08-09 11:03:30 -0300

    stream

--- Stream/src/Match/Main.java
+++ Stream/src/Match/Main.java
@@ -1,21 +1,23 @@
 package Match;
 
-import Reduce.Aluno;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        Match.Aluno a1 = new Match.Aluno("Ana", 7.1);
-        Match.Aluno a2 = new Match.Aluno("Luna", 6.1);
-        Match.Aluno a3 = new Match.Aluno("Gui", 8.1);
-        Match.Aluno a4 = new Match.Aluno("Gabi", 10);
+        Aluno a1 = new Aluno("Ana", 7.1);
+        Aluno a2 = new Aluno("Luna", 7.5);
+        Aluno a3 = new Aluno("Gui", 8.1);
+        Aluno a4 = new Aluno("Gabi", 10);
 
-        List<Match.Aluno> alunos = Arrays.asList(a1,a2,a3,a4);
+        List<Aluno> alunos = Arrays.asList(a1,a2,a3,a4);
 
-        Predicate<Match.Aluno> aprovado = aluno -> aluno.nota >= 7;
+        Predicate<Aluno> aprovado = aluno -> aluno.nota >= 7;
+        Predicate<Aluno> reprovado = aprovado.negate();
 
+        System.out.println(alunos.stream().allMatch(aprovado)); // todos aprovados
+        System.out.println(alunos.stream().anyMatch(aprovado)); // algum aprovado
+        System.out.println(alunos.stream().noneMatch(reprovado)); // nenhum reprovado
     }
 }
