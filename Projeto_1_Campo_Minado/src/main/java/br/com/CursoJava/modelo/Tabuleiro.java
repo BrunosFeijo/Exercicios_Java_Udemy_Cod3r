0ccd32a421ae03952de1df6c2e5593e0372faf9e
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-08-14 19:19:00 -0300

    Campo Minado

--- Projeto_1_Campo_Minado/src/main/java/br/com/CursoJava/modelo/Tabuleiro.java
+++ Projeto_1_Campo_Minado/src/main/java/br/com/CursoJava/modelo/Tabuleiro.java
@@ -1,4 +1,41 @@
 package br.com.CursoJava.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+    private int qtdLinhas;
+    private int qtdColunas;
+    private int qtdMinas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int qtdLinhas, int qtdColunas, int qtdMinas) {
+        this.qtdLinhas = qtdLinhas;
+        this.qtdColunas = qtdColunas;
+        this.qtdMinas = qtdMinas;
+
+        gerarCampos();
+        associarOsVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos() {
+        for (int lin = 0; lin < qtdLinhas; lin++) {
+            for (int col = 0; col < qtdColunas; col++) {
+                campos.add(new Campo(lin, col));
+            }
+        }
+    }
+
+    private void associarOsVizinhos() {
+        for (Campo campo1 : campos) {
+            for (Campo campo2 : campos) {
+                campo1.adicionarVizinho(campo2);
+            }
+        }
+    }
+
+    private void sortearMinas() {
+    }
 }
